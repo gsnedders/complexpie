bc68f203743652f89b2d68187ae7e5aed1e99ec8
Geoffrey Sneddon <geoffers@gmail.com>
2010-02-07 22:03:11 +0100

    Leave HTML elements in no namespace… We'll just take the perf hit elsewhere, I expect, though. :\

--- src/htmlparser.php
+++ src/htmlparser.php
@@ -14,22 +14,6 @@ class HTMLParser
     {
         $dom = new \DOMDocument;
         @$dom->loadHTML($this->data);
-        $elements = $dom->getElementsByTagName('*');
-        foreach ($elements as $element)
-        {
-            $namespace = $element->namespaceURI ?: 'http://www.w3.org/1999/xhtml';
-            $newElement = $dom->createElementNS($namespace, $element->localName);
-            $element->parentNode->insertBefore($newElement, $element);
-            $element->parentNode->removeChild($element);
-            while ($element->firstChild)
-            {
-                $newElement->appendChild($element->firstChild);
-            }
-            foreach ($element->attributes as $attribute)
-            {
-                $newElement->setAttributeNode($attribute);
-            }
-        }
         return $dom;
     }
 }
