42732ae3c4f57e13a20d1a21f741a6945ed0ac9c
Geoffrey Sneddon <geoffers@gmail.com>
2010-02-08 21:22:25 +0100

    Move escaped_html nodes over to a cheaper codepath if possible (i.e., use the basic String content type if they don't contain < or &).

--- src/content.php
+++ src/content.php
@@ -10,13 +10,21 @@ abstract class Content
     
     public static function from_escaped_html($escaped_node)
     {
-        $data = sprintf('<meta http-equiv="content-type" content="text/html;charset=utf-8"><base href="%s"><div>%s', htmlspecialchars($escaped_node->baseURI, ENT_QUOTES, 'UTF-8'), $escaped_node->textContent);
-        $parser = new HTMLParser($data);
-        $dom = $parser->parse();
-        $dom->documentURI = $escaped_node->baseURI; // Useless?
-        $node = $dom->getElementsByTagName('div');
-        $node = $node->item(0);
-        return new Content\Node($node->childNodes);
+        $content = $escaped_node->textContent;
+        if (strpos($content, '<') === false && strpos($content, '&') === false)
+        {
+            return new Content\String($content);
+        }
+        else
+        {
+            $data = sprintf('<meta http-equiv="content-type" content="text/html;charset=utf-8"><base href="%s"><div>%s', htmlspecialchars($escaped_node->baseURI, ENT_QUOTES, 'UTF-8'), $content);
+            $parser = new HTMLParser($data);
+            $dom = $parser->parse();
+            $dom->documentURI = $escaped_node->baseURI; // Useless?
+            $node = $dom->getElementsByTagName('div');
+            $node = $node->item(0);
+            return new Content\Node($node->childNodes);
+        }
     }
     
     abstract public function to_text();
