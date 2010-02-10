6eb9a20c484ec8781fc3e17e05d08ff0682726a4
Geoffrey Sneddon <geoffers@gmail.com>
2010-02-10 01:35:22 +0100

    Remove code now unused now that replaceURL uses the new code.

--- src/misc.php
+++ src/misc.php
@@ -12,62 +12,6 @@ class Misc
             return $relative;
     }
 
-    public static function get_element($realname, $string)
-    {
-        $return = array();
-        $name = preg_quote($realname, '/');
-        if (preg_match_all("/<($name)" . PCRE_HTML_ATTRIBUTE . "(>(.*)<\/$name>|(\/)?>)/siU", $string, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE))
-        {
-            for ($i = 0, $total_matches = count($matches); $i < $total_matches; $i++)
-            {
-                $return[$i]['tag'] = $realname;
-                $return[$i]['full'] = $matches[$i][0][0];
-                $return[$i]['offset'] = $matches[$i][0][1];
-                if (strlen($matches[$i][3][0]) <= 2)
-                {
-                    $return[$i]['self_closing'] = true;
-                }
-                else
-                {
-                    $return[$i]['self_closing'] = false;
-                    $return[$i]['content'] = $matches[$i][4][0];
-                }
-                $return[$i]['attribs'] = array();
-                if (isset($matches[$i][2][0]) && preg_match_all('/[\x09\x0A\x0B\x0C\x0D\x20]+([^\x09\x0A\x0B\x0C\x0D\x20\x2F\x3E][^\x09\x0A\x0B\x0C\x0D\x20\x2F\x3D\x3E]*)(?:[\x09\x0A\x0B\x0C\x0D\x20]*=[\x09\x0A\x0B\x0C\x0D\x20]*(?:"([^"]*)"|\'([^\']*)\'|([^\x09\x0A\x0B\x0C\x0D\x20\x22\x27\x3E][^\x09\x0A\x0B\x0C\x0D\x20\x3E]*)?))?/', ' ' . $matches[$i][2][0] . ' ', $attribs, PREG_SET_ORDER))
-                {
-                    for ($j = 0, $total_attribs = count($attribs); $j < $total_attribs; $j++)
-                    {
-                        if (count($attribs[$j]) === 2)
-                        {
-                            $attribs[$j][2] = $attribs[$j][1];
-                        }
-                        $return[$i]['attribs'][strtolower($attribs[$j][1])]['data'] = Misc::entities_decode(end($attribs[$j]), 'UTF-8');
-                    }
-                }
-            }
-        }
-        return $return;
-    }
-
-    public static function element_implode($element)
-    {
-        $full = "<$element[tag]";
-        foreach ($element['attribs'] as $key => $value)
-        {
-            $key = strtolower($key);
-            $full .= " $key=\"" . htmlspecialchars($value['data']) . '"';
-        }
-        if ($element['self_closing'])
-        {
-            $full .= ' />';
-        }
-        else
-        {
-            $full .= ">$element[content]</$element[tag]>";
-        }
-        return $full;
-    }
-
     public static function error($message, $level, $file, $line)
     {
         if ((ini_get('error_reporting') & $level) > 0)
@@ -107,18 +51,6 @@ class Misc
         }
 
         return $message;
-    }
-
-    /**
-     * Decode HTML entities
-     *
-     * @param string $data Input data
-     * @return string Output data
-     */
-    public static function entities_decode($data)
-    {
-        $decoder = new Decode_HTML_Entities($data);
-        return $decoder->parse();
     }
 
     public static function atom_03_construct_type($attribs)
@@ -213,42 +145,6 @@ class Misc
     public static function is_isegment_nz_nc($string)
     {
         return (bool) preg_match('/^([A-Za-z0-9\-._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!$&\'()*+,;=@]|(%[0-9ABCDEF]{2}))+$/u', $string);
-    }
-
-    /**
-     * Converts a unicode codepoint to a UTF-8 character
-     *
-     * @param int $codepoint Unicode codepoint
-     * @return string UTF-8 character
-     */
-    public static function codepoint_to_utf8($codepoint)
-    {
-        $codepoint = (int) $codepoint;
-        if ($codepoint < 0)
-        {
-            return false;
-        }
-        else if ($codepoint <= 0x7f)
-        {
-            return chr($codepoint);
-        }
-        else if ($codepoint <= 0x7ff)
-        {
-            return chr(0xc0 | ($codepoint >> 6)) . chr(0x80 | ($codepoint & 0x3f));
-        }
-        else if ($codepoint <= 0xffff)
-        {
-            return chr(0xe0 | ($codepoint >> 12)) . chr(0x80 | (($codepoint >> 6) & 0x3f)) . chr(0x80 | ($codepoint & 0x3f));
-        }
-        else if ($codepoint <= 0x10ffff)
-        {
-            return chr(0xf0 | ($codepoint >> 18)) . chr(0x80 | (($codepoint >> 12) & 0x3f)) . chr(0x80 | (($codepoint >> 6) & 0x3f)) . chr(0x80 | ($codepoint & 0x3f));
-        }
-        else
-        {
-            // U+FFFD REPLACEMENT CHARACTER
-            return "\xEF\xBF\xBD";
-        }
     }
     
     public static function get_descendant($root, $query, $namespace_map = array(), $onlyfirst = false)
