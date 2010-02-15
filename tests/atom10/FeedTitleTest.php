ed778b8370059e76d10d98dd0769d1b548f68972
Geoffrey Sneddon <geoffers@gmail.com>
2010-02-15 01:40:32 +0100

    Abstract out the generic text construct tests as much as possible.

---
+++ tests/atom10/FeedTitleTest.php
@@ -0,0 +1,162 @@
+<?php
+
+require_once dirname(__FILE__) . '/TextConstructTest.php';
+require_once dirname(__FILE__) . '/../../src/complexpie.php';
+
+class FeedTitleTest extends TextConstructTest
+{
+    protected function getContent($input)
+    {
+        $input = sprintf("<feed xmlns='http://www.w3.org/2005/Atom'>$input</feed>", 'title');
+        $feed = \ComplexPie\ComplexPie($input);
+        return $feed->title;
+    }
+    
+    public function testNoTitle()
+    {
+        $input = '<feed xmlns="http://www.w3.org/2005/Atom"></feed>';
+        $feed = \ComplexPie\ComplexPie($input);
+        $this->assertSame(null, $feed->title);
+    }
+    
+    public function titlePriorityData()
+    {
+        return array(
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title>PASS</title>
+    <title xmlns="http://purl.org/atom/ns#">FAIL</title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title xmlns="http://purl.org/atom/ns#">FAIL</title>
+    <title>PASS</title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title>PASS</title>
+    <title xmlns="http://purl.org/rss/1.0/">FAIL</title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title xmlns="http://purl.org/rss/1.0/">FAIL</title>
+    <title>PASS</title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title>PASS</title>
+    <title xmlns="http://my.netscape.com/rdf/simple/0.9/">FAIL</title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title xmlns="http://my.netscape.com/rdf/simple/0.9/">FAIL</title>
+    <title>PASS</title>
+</feed>
+EOF
+            ),
+        );
+    }
+    
+    /**
+     * @dataProvider titlePriorityData
+     */
+    public function testTitlePriority($input)
+    {
+        $feed = \ComplexPie\ComplexPie($input);
+        $this->assertSame('PASS', $feed->title->to_text());
+    }
+        
+    public function htmlTitleData()
+    {
+        return array(
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom" xml:base="http://example.com">
+    <title type="html">&lt;a href="/">Test&lt;/a></title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <link href="http://example.com"/>
+    <title type="html">&lt;a href="/">Test&lt;/a></title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title type="html">&lt;a href="/">Test&lt;/a></title>
+    <link href="http://example.com"/>
+</feed>
+EOF
+            ),
+        );
+    }
+    
+    /**
+     * @dataProvider htmlTitleData
+     */
+    public function testHtmlTitle($input)
+    {
+        $feed = \ComplexPie\ComplexPie($input);
+        $this->assertSame('<a href="http://example.com">Test</a>', $feed->title->to_html());
+    }
+        
+    public function xhtmlTitleData()
+    {
+        return array(
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom" xml:base="http://example.com">
+    <title type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml"><a href="/">Test</a></div></title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <link href="http://example.com"/>
+    <title type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml"><a href="/">Test</a></div></title>
+</feed>
+EOF
+            ),
+            array(
+<<<EOF
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml"><a href="/">Test</a></div></title>
+    <link href="http://example.com"/>
+</feed>
+EOF
+            ),
+        );
+    }
+    
+    /**
+     * @dataProvider xhtmlTitleData
+     */
+    public function testXhtmlTitle($input)
+    {
+        $feed = \ComplexPie\ComplexPie($input);
+        $this->assertSame('<a href="http://example.com">Test</a>', $feed->title->to_html());
+    }
+}
+
+?>
---
+++ tests/atom10/TextConstructTest.php
@@ -0,0 +1,173 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+
+abstract class TextConstructTest extends PHPUnit_Framework_TestCase
+{
+    abstract protected function getContent($input);
+    
+    public function basicData()
+    {
+        return array(
+            array('<%1$s>PASS</%1$s>'),
+            array(
+<<<EOF
+<%1\$s>PASS</%1\$s>
+<foo xmlns="http://example.com">
+    <%1\$s>FAIL</%1\$s>
+</foo>
+EOF
+            ),
+            array(
+<<<EOF
+<foo xmlns="http://example.com">
+    <%1\$s>FAIL</%1\$s>
+</foo>
+<%1\$s>PASS</%1\$s>
+EOF
+            ),
+            array(
+<<<EOF
+<%1\$s>PASS</%1\$s>
+<foo:bar xmlns:foo="http://example.com">
+    <%1\$s>FAIL</%1\$s>
+</foo:bar>
+EOF
+            ),
+            array(
+<<<EOF
+<foo:bar xmlns:foo="http://example.com">
+    <%1\$s>FAIL</%1\$s>
+</foo:bar>
+<%1\$s>PASS</%1\$s>
+EOF
+            ),
+        );
+    }
+    
+    /**
+     * @dataProvider basicData
+     */
+    public function testBasic($input)
+    {
+        $this->assertSame('PASS', $this->getContent($input)->to_text());
+    }
+    
+    public function looksLikeHtmlData()
+    {
+        return array(
+            array('<%1$s>&lt;a href="http://example.com">Test&lt;/a></%1$s>'),
+            array('<%1$s>&#x3C;a href="http://example.com">Test&#x3C;/a></%1$s>'),
+            array('<%1$s><![CDATA[<a href="http://example.com">Test</a>]]></%1$s>'),
+            array('<%1$s type="HTML">&lt;a href="http://example.com">Test&lt;/a></%1$s>'),
+            array('<%1$s type="htMl">&lt;a href="http://example.com">Test&lt;/a></%1$s>'),
+            array('<%1$s type=" html ">&lt;a href="http://example.com">Test&lt;/a></%1$s>'),
+            array('<%1$s type="htm">&lt;a href="http://example.com">Test&lt;/a></%1$s>'),
+            array('<%1$s type="totallybogus">&lt;a href="http://example.com">Test&lt;/a></%1$s>'),
+        );
+    }
+    
+    /**
+     * @dataProvider looksLikeHtmlData
+     */
+    public function testLooksLikeHtml($input)
+    {
+        $this->assertSame('<a href="http://example.com">Test</a>', $this->getContent($input)->to_text());
+    }
+    
+    public function htmlData()
+    {
+        return array(
+            array('<%1$s type="html">&lt;a href="http://example.com">Test&lt;/a></%1$s>'),
+            array('<%1$s type="html">&#x3C;a href="http://example.com">Test&#x3C;/a></%1$s>'),
+            array('<%1$s type="html"><![CDATA[<a href="http://example.com">Test</a>]]></%1$s>'),
+            array('<%1$s type="html" xml:base="http://example.com">&lt;a href="/">Test&lt;/a></%1$s>'),
+        );
+    }
+    
+    /**
+     * @dataProvider htmlData
+     */
+    public function testHtml($input)
+    {
+        $this->assertSame('<a href="http://example.com">Test</a>', $this->getContent($input)->to_html());
+    }
+    
+    public function testHtmlInDiv()
+    {
+        $input = '<%1$s type="html" xml:base="http://example.com">&lt;div>&lt;a href="/">Test&lt;/a>&lt;/div></%1$s>';
+        $this->assertSame('<div><a href="http://example.com">Test</a></div>', $this->getContent($input)->to_html());
+    }
+    
+    public function looksLikeXhtmlData()
+    {
+        return array(
+            array('<%1$s><div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div></%1$s>'),
+            array('<%1$s type="XHTML"><div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div></%1$s>'),
+            array('<%1$s type="xhtMl"><div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div></%1$s>'),
+            array('<%1$s type=" xhtml "><div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div></%1$s>'),
+            array('<%1$s type="xht"><div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div></%1$s>'),
+            array('<%1$s type="totallybogus"><div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div></%1$s>'),
+        );
+    }
+    
+    /**
+     * @dataProvider looksLikeXhtmlData
+     */
+    public function testLooksLikeXhtml($input)
+    {
+        $this->assertSame('Test', $this->getContent($input)->to_xml());
+    }
+
+    public function xhtmlData()
+    {
+        return array(
+            array('<%1$s type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div></%1$s>'),
+            array('<%1$s type="xhtml" xml:base="http://example.com"><div xmlns="http://www.w3.org/1999/xhtml"><a href="/">Test</a></div></%1$s>'),
+            array(
+<<<EOF
+<%1\$s type="xhtml">
+    <div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div>
+</%1\$s>
+EOF
+            ),
+            array(
+<<<EOF
+<%1\$s type="xhtml">
+    <div xmlns="http://www.w3.org/1999/xhtml">
+        <a href="http://example.com">Test</a>
+    </div>
+</%1\$s>
+EOF
+            ),
+            array('<%1$s type="xhtml"><!--foo--><div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div></%1$s>'),
+        );
+    }
+    
+    /**
+     * @dataProvider xhtmlData
+     */
+    public function testXhtml($input)
+    {
+        $this->assertSame('<a href="http://example.com">Test</a>', $this->getContent($input)->to_html());
+    }
+
+    public function escapedXhtmlData()
+    {
+        return array(
+            array('<%1$s type="xhtml">&lt;div xmlns="http://www.w3.org/1999/xhtml">&lt;a href="http://example.com">Test&lt;/a>&lt;/div></%1$s>'),
+            array('<%1$s type="xhtml">&#x3C;div xmlns="http://www.w3.org/1999/xhtml">&#x3C;a href="http://example.com">Test&#x3C;/a>&#x3C;/div></%1$s>'),
+            array('<%1$s type="xhtml"><![CDATA[<div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div>]]></%1$s>'),
+        );
+    }
+    
+    /**
+     * @dataProvider escapedXhtmlData
+     */
+    public function testEscapedXhtml($input)
+    {
+        $this->assertSame('<div xmlns="http://www.w3.org/1999/xhtml"><a href="http://example.com">Test</a></div>', $this->getContent($input)->to_text());
+    }
+}
+
+?>
