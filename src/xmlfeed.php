014e0f0b1242f5ee48f67bd8cbebdcd01f5ea733
Geoffrey Sneddon <geoffers@gmail.com>
2010-03-13 16:45:58 +0100

    Remember the magic property we need on all subclasses of Extension.

--- src/xmlfeed.php
+++ src/xmlfeed.php
@@ -3,9 +3,9 @@ namespace ComplexPie;
 
 class XMLFeed extends Feed
 {
+    protected static $static_ext = array();
 }
 
 /* RSS 2.0 is not included here as we don't want to pick up the non-namespaced
    elements unless we are treating the tree as RSS 2.0. */
-
 XMLFeed::add_static_extension('get', '\\ComplexPie\\Atom10\\Feed::get', 10, true);
