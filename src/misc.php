5ccc9d53160fa2d48e019319ff46fb6f5cd972ab
Geoffrey Sneddon <geoffers@gmail.com>
2010-03-19 20:11:58 +0100

    Remove old, dead code.

--- src/misc.php
+++ src/misc.php
@@ -89,59 +89,6 @@ class Misc
         }
     }
 
-    public static function atom_10_construct_type($attribs)
-    {
-        if (isset($attribs['']['type']))
-        {
-            switch (strtolower(trim($attribs['']['type'])))
-            {
-                case 'text':
-                    return CONSTRUCT_TEXT;
-
-                case 'html':
-                    return CONSTRUCT_HTML;
-
-                case 'xhtml':
-                    return CONSTRUCT_XHTML;
-
-                default:
-                    return CONSTRUCT_NONE;
-            }
-        }
-        return CONSTRUCT_TEXT;
-    }
-
-    public static function atom_10_content_construct_type($attribs)
-    {
-        if (isset($attribs['']['type']))
-        {
-            $type = strtolower(trim($attribs['']['type']));
-            switch ($type)
-            {
-                case 'text':
-                    return CONSTRUCT_TEXT;
-
-                case 'html':
-                    return CONSTRUCT_HTML;
-
-                case 'xhtml':
-                    return CONSTRUCT_XHTML;
-            }
-            if (in_array(substr($type, -4), array('+xml', '/xml')) || substr($type, 0, 5) === 'text/')
-            {
-                return CONSTRUCT_NONE;
-            }
-            else
-            {
-                return CONSTRUCT_BASE64;
-            }
-        }
-        else
-        {
-            return CONSTRUCT_TEXT;
-        }
-    }
-
     public static function is_isegment_nz_nc($string)
     {
         return (bool) preg_match('/^([A-Za-z0-9\-._~\x{A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}\x{10000}-\x{1FFFD}\x{20000}-\x{2FFFD}\x{30000}-\x{3FFFD}\x{40000}-\x{4FFFD}\x{50000}-\x{5FFFD}\x{60000}-\x{6FFFD}\x{70000}-\x{7FFFD}\x{80000}-\x{8FFFD}\x{90000}-\x{9FFFD}\x{A0000}-\x{AFFFD}\x{B0000}-\x{BFFFD}\x{C0000}-\x{CFFFD}\x{D0000}-\x{DFFFD}\x{E1000}-\x{EFFFD}!$&\'()*+,;=@]|(%[0-9ABCDEF]{2}))+$/u', $string);
